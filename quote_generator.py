5fb46be3f40734377ab45acd71b3a75a669663a6
root <root@ip-172-31-9-178.ap-south-1.compute.internal>
2025-02-14 12:19:08 +0000

    new

--- quote_generator.py
+++ quote_generator.py
@@ -18,4 +18,4 @@ def home():
     return render_template('index.html', quote=random_quote)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
